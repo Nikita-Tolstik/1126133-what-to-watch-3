089903b8ccb8bdc140bb7a10430ff77b5306d2e3
Nikita <tolstik-nik@mail.ru>
2020-02-09 15:45:13 +0300

    Создал массив названий, отрисовал список фильмов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Main from '../main/main.jsx';
 
-const App = (props) => {
+// eslint-disable-next-line react/prop-types
+const App = ({title, genre, year, titles}) => {
 
-  // eslint-disable-next-line react/prop-types
-  const {title, genre, year} = props;
 
   return (
     <Main
       title={title}
       genre={genre}
       year={year}
+      titles={titles}
     />
   );
 };
