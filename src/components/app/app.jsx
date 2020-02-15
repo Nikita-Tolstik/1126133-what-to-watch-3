478dae7c85e59bc95d5e708a480fdb2b84fded3d
Nikita <tolstik-nik@mail.ru>
2020-02-15 19:10:25 +0300

    Сделал App.js классовым

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import Main from '../main/main.jsx';
-import PropTypes from 'prop-types';
+import PropTypes, {shape} from 'prop-types';
 
 const handleTitleClick = (evt) => {
   evt.preventDefault();
 };
 
-const App = ({title, genre, year, titles}) => {
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <Main
-      title={title}
-      genre={genre}
-      year={year}
-      titles={titles}
-      onTitleClick={handleTitleClick}
-    />
-  );
-};
+  }
+
+  render() {
+    const {title, genre, year, films} = this.props;
+
+    return (
+      <Main
+        title={title}
+        genre={genre}
+        year={year}
+        films={films}
+        onTitleClick={handleTitleClick}
+      />
+    );
+  }
+}
+
 
 App.propTypes = {
-
   title: PropTypes.string.isRequired,
   genre: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
-  titles: PropTypes.arrayOf(
-      PropTypes.string.isRequired
-  ).isRequired
+  films: PropTypes.arrayOf(shape({
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })).isRequired,
 };
 
 export default App;
