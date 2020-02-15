e8508cfb6c7ff8333ec0299cec11902f6c40abb7
Nikita <tolstik-nik@mail.ru>
2020-02-15 23:26:46 +0300

    Сделал snapshot тесты для новых компонентов

---
+++ src/components/films-list/films-list.test.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import FilmsList from './films-list.jsx';
+
+const mock = [
+  {
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+  },
+  {
+    img: `img/macbeth.jpg`,
+    title: `Macbeth`,
+  },
+  {
+    img: `img/aviator.jpg`,
+    title: `Aviator`,
+  },
+  {
+    img: `img/revenant.jpg`,
+    title: `Revenant`,
+  },
+  {
+    img: `img/johnny-english.jpg`,
+    title: `Johnny English`,
+  },
+  {
+    img: `img/snatch.jpg`,
+    title: `Snatch`,
+  },
+  {
+    img: `img/mindhunter.jpg`,
+    title: `Mindhunter`,
+  },
+  {
+    img: `img/war-of-the-worlds.jpg`,
+    title: `War of the worlds`,
+  },
+];
+
+it(`Render <FilmsList />`, () => {
+
+  const tree = renderer.create(
+      <FilmsList
+
+        films={mock}
+        onTitleClick={() => {}}
+      />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
