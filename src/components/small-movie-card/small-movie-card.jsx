73fd439990b04a9d58d91310ee8c2b432a18a7ae
Nikita <tolstik-nik@mail.ru>
2020-02-15 22:56:31 +0300

    Передал обработчики наведения

--- src/components/small-movie-card/small-movie-card.jsx
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import PropTypes, {shape} from 'prop-types';
+import PropTypes from 'prop-types';
 
-const SmallMovieCard = ({film, onTitleClick, onMouseCardFilm}) => {
+const SmallMovieCard = ({film, onTitleClick, onMouseEnterFilm, onMouseLeaveFilm}) => {
 
   return (
 
     <article
-
-      onMouseOver={() => onMouseCardFilm(film)}
+      onMouseEnter={() => onMouseEnterFilm(film)}
+      onMouseLeave={onMouseLeaveFilm}
       className="small-movie-card catalog__movies-card">
 
       <div className="small-movie-card__image">
         <img src={film.img} alt={film.title} width="280" height="175" />
       </div>
+
       <h3 className="small-movie-card__title">
-        <a onClick={onTitleClick} className="small-movie-card__link" href="movie-page.html">
+
+        <a onClick={onTitleClick}
+          className="small-movie-card__link" href="movie-page.html">
           {film.title}
         </a>
       </h3>
@@ -23,12 +26,13 @@ const SmallMovieCard = ({film, onTitleClick, onMouseCardFilm}) => {
 };
 
 SmallMovieCard.propTypes = {
-  film: shape({
+  film: PropTypes.shape({
     img: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
   }).isRequired,
   onTitleClick: PropTypes.func.isRequired,
-  onMouseCardFilm: PropTypes.func.isRequired,
+  onMouseEnterFilm: PropTypes.func.isRequired,
+  onMouseLeaveFilm: PropTypes.func.isRequired,
 };
 
 export default SmallMovieCard;
