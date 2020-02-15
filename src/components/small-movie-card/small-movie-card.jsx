ba8c8b7617354c5c416348392da73f3f2198f60e
Nikita <tolstik-nik@mail.ru>
2020-02-15 19:07:55 +0300

    Создал отдельный компонент карточки фильма из списка

---
+++ src/components/small-movie-card/small-movie-card.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes, {shape} from 'prop-types';
+
+const SmallMovieCard = ({film, onTitleClick}) => {
+
+  return (
+
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={film.img} alt={film.title} width="280" height="175" />
+      </div>
+      <h3 className="small-movie-card__title">
+        <a
+          onClick={onTitleClick}
+          className="small-movie-card__link" href="movie-page.html">{film.title}</a>
+      </h3>
+    </article>
+  );
+};
+
+SmallMovieCard.propTypes = {
+  film: shape({
+    img: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  onTitleClick: PropTypes.func.isRequired
+};
+
+export default SmallMovieCard;
