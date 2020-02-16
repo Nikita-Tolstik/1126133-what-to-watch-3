4bd1fd61ed8c32ab4bf6f6f93b6c9c4bdbeb5473
Nikita <tolstik-nik@mail.ru>
2020-02-16 22:48:09 +0300

    Экспортировал компонент movie-page

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -140,21 +140,24 @@ const MoviePage = () => {
             </article>
           </div>
         </section>
+
+
+        <footer className="page-footer">
+          <div className="logo">
+            <a href="main.html" className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="copyright">
+            <p>© 2019 What to watch Ltd.</p>
+          </div>
+        </footer>
       </div>
-
-      <footer className="page-footer">
-        <div className="logo">
-          <a href="main.html" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
     </React.Fragment>
   );
 };
+
+export default MoviePage;
