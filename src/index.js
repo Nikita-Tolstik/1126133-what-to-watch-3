25264b94e17672e8fe6ba12d5807898797298e59
Nikita <tolstik-nik@mail.ru>
2020-02-17 00:28:10 +0300

    Поправил импорт

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
-import films from './mocks/films.js';
+import {films} from './mocks/films.js';
 
 const Settings = {
   TITLE: `The Grand Budapest Hotel`,
