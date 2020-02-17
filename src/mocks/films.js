f238407d8b524d7c630a8335a523ff4e96cea5c8
Nikita <tolstik-nik@mail.ru>
2020-02-18 00:24:50 +0300

    Поправил константы и моки

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,5 +1,5 @@
 import {getRandomNumber, getRatingNumber} from '../utils/utils.js';
-import {MOCKS, DESCRIPTION, DIRECTOR, STARRING, GENRES} from '../const.js';
+import {MOCKS, DESCRIPTIONS, DIRECTOR, STARRING, GENRES} from '../const.js';
 
 export const films = MOCKS.map((it) => {
 
@@ -8,7 +8,7 @@ export const films = MOCKS.map((it) => {
     img: it.img,
     genre: GENRES[getRandomNumber(0, GENRES.length - 1)],
     year: getRandomNumber(1970, 2020),
-    description: DESCRIPTION,
+    description: DESCRIPTIONS,
     rating: getRatingNumber(1, 10),
     quantityRatings: getRandomNumber(10, 300),
     director: DIRECTOR,
